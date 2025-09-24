429320109fd947322bbff1374403a1e2a851f201
emmabhl <emma.boehly@epfl.ch>
2025-09-23 22:36:49 -0400

    correction run directory

--- train_yolo.sh
+++ train_yolo.sh
@@ -15,14 +15,11 @@ module load gcc python opencv/4.12.0
 # Activate virtual environment
 source scale/bin/activate
 
-# Navigate to the project directory
-cd src
-
 # Step 1: Get the data
-python get_data.py --path original1/scalebar-dataset --data_dir data
+python src/get_data.py --path original1/scalebar-dataset --data_dir data
 
 # Step 2: Convert JSONs to YOLO format
-python convert_jsons_to_yolo.py --data_dir data --validate
+python src/convert_jsons_to_yolo.py --data_dir data --validate
 
 # Step 3: Train the YOLO model
-python train_yolo.py --data_yaml data/data.yaml --model_name yolov8m.pt --epochs 500 --batch 16
+python src/train_yolo.py --data_yaml data/data.yaml --model_name yolov8m.pt --epochs 500 --batch 16 --device 0
